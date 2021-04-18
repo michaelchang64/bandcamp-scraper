6c794f0bfd7dbd29edd9a062359bca41215e15e3
michaelchang64 <changmichael64@gmail.com>
2021-04-18 16:25:57 -0400

    revised all code, updated to new bandcamp json structure

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-# Janky Bandcamp Web Scraper
+# More Legit and Certainly Not Janky Bandcamp Web Scraper
 
 This bad boy can download so many mp3's from Bandcamp!!!
 
-I wrote this while procrastinating on my other psets because learning how to webscrape with Python was *clearly* more important.
-
-After you download all the dependencies (actually just `pip install bs4` and you're good lmao), Run it like so:
-```
-python3 bandcamp_scrape.py "your_url.bandcamp.com/blahblah" "/file/path/to/store/mp3s/in"
-```
+## Disclaimer
 
 Please don't arrest me or report me for piracy, I only did this for educational purposes.
 
 Also, if you `insPecT thE ElEmenTS` carefully enough, you'll see that Bandcamp has a [lax policy](https://bandcamp.com/zendesk/help/audio_basics#steal).
 
 Hope you enjoy this! DO SUPPORT GROWING ARTISTS BY BUYING THEIR MUSIC!!!!!!!
+
+## Set-up
+1. `git clone` this repo to a known directory on your local system
+2. Create your virtual environment within repo (we'll call ours `venv`): `python -m venv venv`
+3. Install all necessary dependencies: `pip install -r requirements.txt`
+4. Now, you should be set up to run the scrape!
+    * Make sure the first argument after `python bandcamp_scrape.py` is the bandcamp album url
+    * You can specify a local file path and it will create a folder if it doesn't already exist
+        * If you write to a folder that already exists, it will delete everything in it and overwrite, so beware!
+    * See template and example below:
+```
+python bandcamp_scrape.py "artist_name.bandcamp.com/album/album_name" "./some_file_path"
+```
+
+## Example
+
+```
+python bandcamp_scrape.py "https://music.midwestcollective.us/album/odyssey" "./odyssey"
+```
--- bandcamp_scrape.py
+++ bandcamp_scrape.py
@@ -1,59 +1,87 @@
-import os, sys, requests, json, re
+import os
+import sys
+import requests
+import json
+import re
 from bs4 import BeautifulSoup
+
+###################
+### SYSTEM INIT ###
+###################
 
 # redirects things going to stdout to filename
 sys.stdout = open('stdout.txt', 'w')
 
 # take command line args, url then directory name, respectively
-url = sys.argv[1]
-download_dir = sys.argv[2]
+url = sys.argv[1] # first command line arg, after 'python'
+download_dir = sys.argv[2] # second arg, download directory
+
 # if dir DNE, make dir, else leave as is (doesn't remake directory!!!)
 os.makedirs(download_dir, exist_ok=True)  # succeeds even if directory exists.
 
+####################
+### REQUEST STEP ###
+####################
+
+# spoof header to make requester seem more human
+header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"}
 # gets url webpage content
-result = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"})
+bandcamp_page = requests.get(url, headers=header)
+bandcamp_page_src = bandcamp_page.content
 
-# src is result content, soup is parsed src object
-src = result.content
-soup = BeautifulSoup(src, 'html.parser')
+###################
+### SCRAPE STEP ###
+###################
 
-# script then finds script tag containing string 'TralbumData', which
+# bandcamp_page_src is result content, then parsed by soup
+soup = BeautifulSoup(bandcamp_page_src, 'lxml')
+
+# find script tag containing type 'application/ld+json', which
 # is an array of JSON that contains data, including links to mp3s
-script = soup.find('script', text=re.compile('TralbumData'))
-# data is everything within var TralbumData
-data = re.search(r"var TralbumData =(.+?);\n", script.text, re.S).group(0)
-# apparently contents within trackinfo is randomized/unordered, so to
-# get this nested array, must first substring to trackinfo then 
-# substring to nested array containing album data
-data = data[data.index("trackinfo"):data.index("]")] + "]"
-data = data[data.index("["):]
+json_script_obj = soup.find('script', {'type': 'application/ld+json'})
 
-# for debugging, print data
-print(data, "\n")
+# load data from json obj
+json_script_obj_src = json_script_obj.contents[0]
+raw_json = json.loads(json_script_obj_src)
 
-# if data exists, then parse data into Python-readable dict
-# otherwise return error
-if data:
-    json_data = json.loads(data)
-else:
-    print("Error: no such data oof")
+# find pertinent list with track data
+track_data = raw_json['track']['itemListElement']
 
-# for debugging, print json_data to see if captured correctly
-print(type(json_data))
-print(json_data, "\n")
-# url of the first track in album
-print(json_data[0]["file"]["mp3-128"])
+#####################
+### DOWNLOAD STEP ###
+#####################
 
 print('Beginning file download with requests\n')
 
-# iterate through array of dicts to download each mp3
-for mp3 in json_data:
-    mp3_url = mp3["file"]["mp3-128"]
-    r = requests.get(mp3_url, "\n")
-    print(r.status_code)
-    mp3_title = download_dir + "/" + mp3["title"] + ".mp3"
-#     procedure to download mp3 in target folder
-    with open(mp3_title, 'wb') as f:
-        f.write(r.content)
+# get number of tracks to download
+num_tracks = len(track_data)
+i = 0
 
-print("Task finished")
+# iterate through array of dicts to download each mp3
+for mp3 in track_data:
+    # get pertinent metadata from each track in list
+    mp3_title = mp3['item']['name']
+    mp3_url = mp3['item']['additionalProperty'][2]['value']
+
+    # get page with raw mp3
+    print('Extracting mp3 from', mp3_title, 'at', mp3_url)
+    mp3_page = requests.get(mp3_url, "\n")
+
+    # increment track number by one
+    i += 1
+
+    # conditions to check if mp3_page is accessible
+    if mp3_page.ok:
+        # title each mp3 based on title
+        title = download_dir + "/" + mp3_title + ".mp3"
+
+        # download mp3 into target folder
+        with open(title, 'wb') as f:
+            f.write(mp3_page.content)
+
+        print('Downloaded track', i, 'of', num_tracks, '\n')
+    else:
+        print('mp3 track', i, 'not accessible, error code', mp3_page.status_code)
+        # print('mp3 track', 'not accessible, error code', mp3_page.status_code)
+
+print("\nAll done!")
